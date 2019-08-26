f54c825c0ba923235618c386420faff67c3bbea9
lijiong16 <lijiong16@github.com>
2019-08-26 09:08:06 +0800

    add results

--- core/config.py
+++ core/config.py
@@ -45,21 +45,20 @@ __C.TRAIN.LEARN_RATE_END        = 1e-7
 __C.TRAIN.WARMUP_EPOCHS         = 1#2
 __C.TRAIN.FISRT_STAGE_EPOCHS    = 0 #20
 __C.TRAIN.SECOND_STAGE_EPOCHS   = 30
-__C.TRAIN.INITIAL_WEIGHT        = "./checkpoint/yolov3_test_loss=12.9836.ckpt-4"#"./checkpoint/yolov3_coco_demo.ckpt"
+__C.TRAIN.INITIAL_WEIGHT        = "./checkpoint/yolov3_test_loss=7.7501.ckpt-1"#"./checkpoint/yolov3_coco_demo.ckpt"
 
 
 
 # TEST options
 __C.TEST                        = edict()
-
-__C.TEST.ANNOT_PATH             = "./data/dataset/low_test.txt"
+__C.TEST.ANNOT_PATH             = "./data/dataset/low_test.txt"#"./data/dataset/low_test.txt"
 __C.TEST.BATCH_SIZE             = 2
 __C.TEST.INPUT_SIZE             = 544
 __C.TEST.DATA_AUG               = False
 __C.TEST.WRITE_IMAGE            = True
 __C.TEST.WRITE_IMAGE_PATH       = "./data/detection/"
 __C.TEST.WRITE_IMAGE_SHOW_LABEL = True
-__C.TEST.WEIGHT_FILE            = "./checkpoint/yolov3_test_loss=10.2566.ckpt-7"#"./checkpoint/yolov3_coco_demo.ckpt"#
+__C.TEST.WEIGHT_FILE            = "./checkpoint/clean/yolov3_test_loss=7.2754.ckpt-7"#"./checkpoint/yolov3_coco_demo.ckpt"#
 __C.TEST.SHOW_LABEL             = True
 __C.TEST.SCORE_THRESHOLD        = 0.3
 __C.TEST.IOU_THRESHOLD          = 0.45
